52e11b96a6797565a972be2ce9fa36535fc34a14
Mikhail Vaysman <Vaysman@users.noreply.github.com>
2024-11-20 12:20:59 +0200

    switch back to IDENTITY strategy

--- src/main/java/com/example/spdockpro/entity/EmployeeEntity.java
+++ src/main/java/com/example/spdockpro/entity/EmployeeEntity.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "employees")
 public class EmployeeEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
     @Column(name = "name")
